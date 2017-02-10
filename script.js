0a95d3b155487bb87cbc8a9445ad24b70e89439b
Your Name <you@example.com>
2017-02-10 21:25:32 +0000

    zadanie js zmienne trojkat

---
+++ script.js
@@ -0,0 +1,6 @@
+var a = prompt('Wpisz swoje liczbę:');
+var h=prompt('Wpisz swoje liczbę:');
+var triangleArea ;
+triangleArea =a*h/2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log(triangleArea);
